fc8a749d539baa2078931370f7c5433d5b10c54a
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-07-30 17:58:17 +0530

    Initial setup for portfolio project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatModule } from './appModules/mat.module';
+import { HeaderComponent } from './includes/header/header.component';
+import { FooterComponent } from './includes/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    ProjectsComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/appModules/mat.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const material: any = [
+
+]
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        material
+    ], 
+    exports: [
+        material
+    ]
+})
+export class MatModule { }

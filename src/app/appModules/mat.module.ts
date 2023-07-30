4874e71eb8ee05136419ec7ea9f45d0853c4398b
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-07-30 21:13:50 +0530

    Navbar modified with the nav options

--- src/app/appModules/mat.module.ts
+++ src/app/appModules/mat.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-const material: any = [
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+
+const material: any = [
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatToolbarModule
 ]
 
 @NgModule({

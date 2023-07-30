d6b97ae8517c4968dfbd1517661d5c700262bff6
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-07-30 21:29:38 +0530

    Header routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact-me', component: ContactMeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
